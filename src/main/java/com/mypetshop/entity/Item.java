3cf8332ce72d3cc2196925971528bbcb903057cd
1155476 <hanyd1012@hyundai-autoever.com>
2025-03-22 14:16:47 +0900

    item dto, controller commit revise

--- src/main/java/com/mypetshop/entity/Item.java
+++ src/main/java/com/mypetshop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.mypetshop.entity;
 
 import com.mypetshop.constant.ItemSellStatus;
+import com.mypetshop.dto.ItemFormDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -40,5 +41,13 @@ public class Item {
     private LocalDateTime regTime;
 
     private LocalDateTime updateTime;
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 
 }
--- src/main/java/com/mypetshop/service/ItemService.java
+++ src/main/java/com/mypetshop/service/ItemService.java
@@ -1,15 +1,18 @@
 package com.mypetshop.service;
 
 import com.mypetshop.dto.ItemFormDto;
+import com.mypetshop.dto.ItemImgDto;
 import com.mypetshop.entity.Item;
 import com.mypetshop.entity.ItemImg;
 import com.mypetshop.repository.ItemRepository;
-import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -36,6 +39,39 @@ public class ItemService {
                 itemImg.setRepimgYn("N");
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
         }
+        return item.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId) {
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+    }
+
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+
+        //상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDto);
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();
+
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++){
+            itemImgService.updateItemImg(itemImgIds.get(i),
+                    itemImgFileList.get(i));
+        }
+
         return item.getId();
     }
 }
